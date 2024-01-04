60cd9f09726c8ee37f120296d9448e1368e90bf5
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-01-04 20:09:57 +0900

    node server -> express server

--- express-server/index.js
+++ express-server/index.js
@@ -1,13 +1,29 @@
+const url = require('url')
 const express = require('express')
 const app = express()
 
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.set({ 'Content-Type': 'text/html; charset=utf-8' })
-  res.end('hello Express')
+  res.json('HOME')
 })
+
+app.get('/user', user)
+app.get('/feed', feed)
 
 app.listen(port, () => {
   console.log(`START SERVER : use ${port}`)
 })
+
+function user(req, res) {
+  const userInfo = url.parse(req.url, true).query
+  res.json(`[user] name: ${userInfo.user}, age: ${userInfo.age}`)
+}
+
+function feed(req, res) {
+  res.json(`<ul>
+    <li>picture1</li>
+    <li>picture2</li>
+    <li>picture3</li>
+  </ul>`)
+}
