6970391d4e3c4975cf03752f8dc4527fe8d8df9e
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-01-05 23:51:34 +0900

    간단한 게시판 API

--- express-server/index.js
+++ express-server/index.js
@@ -4,26 +4,44 @@ const app = express()
 
 const port = 3000
 
+let posts = []
+
+app.use(express.json()) // body를 사용하기 위해 활성화
+app.use(express.urlencoded({ extended: true })) // POST 요청 시 컨텐트 타입이 application/x-www-urlendcoded인 경우 파싱
+
 app.get('/', (req, res) => {
-  res.json('HOME')
+  res.json(posts)
 })
 
-app.get('/user', user)
-app.get('/feed', feed)
+app.post('/posts', (req, res) => {
+  const { title, name, text } = req.body
+  const newPost = {
+    id: posts.length + 1,
+    title,
+    text,
+    name,
+    createdAt: Date(),
+  }
+
+  posts.push(newPost)
+
+  res.json(newPost)
+})
+
+app.delete('/posts/:id', (req, res) => {
+  const id = req.params.id
+
+  const filteredPosts = posts.filter((post) => post.id !== +id) // +숫자로 변환
+  const isLengthChanged = filteredPosts.length !== posts.length
+  posts = filteredPosts
+
+  if (isLengthChanged) {
+    res.json("OK")
+    return
+  }
+  res.json("NOT CHANGED")
+})
 
 app.listen(port, () => {
-  console.log(`START SERVER : use ${port}`)
+  console.log(`START POST SERVER : use ${port}`)
 })
-
-function user(req, res) {
-  const userInfo = url.parse(req.url, true).query
-  res.json(`[user] name: ${userInfo.user}, age: ${userInfo.age}`)
-}
-
-function feed(req, res) {
-  res.json(`<ul>
-    <li>picture1</li>
-    <li>picture2</li>
-    <li>picture3</li>
-  </ul>`)
-}
